fe36b1cbe0b7eeb859c27ea8b9240bce10d3287b
Matsuda Shoichi <matsuda@microcms.co.jp>
2022-09-16 17:57:49 +0900

    Initial Commit

---
+++ src/library/microcms.ts
@@ -0,0 +1,38 @@
+//SDK利用準備
+import { createClient, MicroCMSQueries } from "microcms-js-sdk";
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+//型定義
+export type Blog = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+};
+export type BlogResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Blog[];
+};
+
+//APIの呼び出し
+export const getBlogs = async (queries?: MicroCMSQueries) => {
+  return await client.get<BlogResponse>({ endpoint: "blogs5", queries });
+};
+export const getBlogDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Blog>({
+    endpoint: "blogs5",
+    contentId,
+    queries,
+  });
+};
